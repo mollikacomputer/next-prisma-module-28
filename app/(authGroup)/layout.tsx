30e5216274dee9c175d66333311b1f89cbe51c88
mollikacomputer <mollikacomputer3@gmail.com>
2026-07-25 09:46:42 +0600

    implement login form

--- app/(authGroup)/layout.tsx
+++ app/(authGroup)/layout.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const AuthLayout = ({children}:{children: React.ReactNode}) => {
   return (
-    <>
+    // <div className='flex justify-cente'>
+    <div className='max-w-7xl mx-auto'>
       {children}
-    </>
+    </div>
   )
 }
 
